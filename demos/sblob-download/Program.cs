6fde1c8910af9bcb7cd198bfb1a32395bd143853
Mahendra shinde <MahendraShinde@synergetics-india.com>
2026-02-23 12:18:50 +0530

    Add initial implementation for Azure Blob Storage download demo

---
+++ demos/sblob-download/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Azure.Storage.Blobs;
+
+string connectionString = "<CONNECTION_STRING>";
+string containerName = "files";
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+string blobName = "product.csv";
+
+BlobClient blobClient = containerClient.GetBlobClient(blobName);
+string localFilePath = Path.Combine(Environment.CurrentDirectory, blobName);
+Console.WriteLine($"Downloading blob to {localFilePath}");
+await blobClient.DownloadToAsync(localFilePath);
